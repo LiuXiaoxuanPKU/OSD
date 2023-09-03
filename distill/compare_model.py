2243a2d3278f4ac75995c68e551d1daafc7fc0e0
LiuXiaoxuanPKU <lilyliupku@gmail.com>
2023-09-03 04:10:48 +0000

    merge

--- distill/compare_model.py
+++ distill/compare_model.py
@@ -35,35 +35,59 @@ def main(student_model_path, teacher_model_path,
         stats[correct_tokens] = stats[correct_tokens] + 1
         if i % 10 == 0:
             print(f"{i}/{len(eval_dataset)}")
-        # print("===================================")
-        # print(tokenizer.decode(d["input_ids"]))
-        # print(output)
-        # print(correct_tokens.shape)
-        # print(propose_step, correct_tokens.shape[-1]/propose_step)
-        # correctness += correct_tokens.shape[-1]/propose_step
-        # print("===================================")
+        print("===================================")
+        print(tokenizer.decode(d["input_ids"]))
+        print(output)
+        print(correct_tokens.shape)
+        print(propose_step, correct_tokens.shape[-1]/propose_step)
+        print(tokenizer.batch_decode(correct_tokens))
+        correctness += correct_tokens.shape[-1]/propose_step
+        print("===================================")
         i += 1
+        if i == 10:
+            exit(0)
     print(i, correctness / i)
     
-    with open('output/llama_correct.pkl', 'wb') as f:
+    with open('output/vicuna-7k_correct.pkl', 'wb') as f:
         pickle.dump(stats, f)
 
 
+def model_generate(model_path, data_path):
+    tokenizer = AutoTokenizer.from_pretrained("/data/vicuna-7b-v1.3/")
+    tokenizer.pad_token = tokenizer.unk_token
+    model = load_model(model_path)
+    eval_json = json.load(open(data_path, "r"))
+    eval_dataset = LazySupervisedDataset(eval_json, tokenizer=tokenizer, 
+                                         model=model_path, do_eval=True)
+    
+    i = 0
+    for d in eval_dataset:
+        max_tokens = 100
+        input_ids = d["input_ids"].reshape(1, -1).cuda()
+        generated = model.generate(input_ids, max_new_tokens=max_tokens)[0][input_ids.shape[-1]:]
+        print(f"Prompt: {tokenizer.decode(input_ids[0])}")
+        print(f"Answer: {tokenizer.decode(generated)}")
+        print("----------------------------------")
+        i += 1
+        if i == 10:
+            break
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--student", type=str, 
                         help="student model path", 
-                        default="/rscratch/zhendong/lily/llama-160m/")
-    # parser.add_argument("--teacher", type=str, 
-    #                     help="teacher model path", 
-    #                     default="/rscratch/zhendong/lily/vicuna-7b-v1.3/")
+                        default="eqhylxx/vicuna-160m")
     parser.add_argument("--teacher", type=str, 
                         help="teacher model path", 
-                        default="/rscratch/zhendong/lily/llama-7b/")
+                        default="/data/vicuna-7b-v1.3/")
+    # parser.add_argument("--teacher", type=str, 
+    #                     help="teacher model path", 
+    #                     default="/data/vicuna_cip/checkpoint-7000")
     
     parser.add_argument("--data", type=str, 
                         help="data path", 
-                        default="/home/eecs/zhen/lily/spec/data/cip_eval.json")
+                        default="/home/lily/specNBCE/data/spider_eval.json")
     
     args = parser.parse_args()
-    main(args.student, args.teacher, args.data)
+    # main(args.student, args.teacher, args.data)
+    model_generate(args.student, args.data)
