reverse kl partial fix, still buggy

distill/distill_trainer.py
@@ -51,9 +51,9 @@ class DistillTrainer(Trainer):
         return loss.detach()
 
     def training_step(self, model, inputs):
-        max_new_tokens = 512
+        max_new_tokens = 128
         temperature = 1
-        use_kl = True
+        use_kl = False
         if use_kl:
             with torch.no_grad():
                 teacher_outputs = self.teacher_model.generate(
@@ -90,32 +90,47 @@ class DistillTrainer(Trainer):
                                                          generated_token_ids,
                                                          ignore_index=self.tokenizer.pad_token_id)
         else:
-            student_outputs = model(
-                input_ids=inputs['input_ids'],
-                attention_mask=inputs['attention_mask'],
-                max_new_tokens=max_new_tokens,
-                output_scores=True,
-                return_dict_in_generate=True)
-            student_logits = torch.cat(
-                [score.unsqueeze(1) for score in student_outputs["scores"]], dim=1)
-
+            with torch.no_grad():
+                student_outputs = model.generate(
+                    input_ids=inputs['input_ids'],
+                    attention_mask=inputs['attention_mask'],
+                    max_new_tokens=max_new_tokens,
+                    return_dict_in_generate=True)
+        
             bsz, total_seq_len = student_outputs["sequences"].shape
-            gen_len = len(student_outputs["scores"])
+            gen_len = total_seq_len - inputs['input_ids'].shape[-1]
+            generated_token_ids = student_outputs["sequences"][:, -gen_len:]
+            output_mask = generated_token_ids != self.tokenizer.pad_token_id
             attention_mask = torch.cat([inputs['attention_mask'],
-                                        torch.ones((bsz, gen_len), device='cuda', dtype=torch.long)], dim=1)
+                                        torch.ones((bsz, gen_len), device='cuda', dtype=torch.long)], dim=1)     
+            student_logits = model(
+                input_ids=student_outputs["sequences"],
+                attention_mask=attention_mask,
+                ).logits[:, -gen_len-1:-1, :]
 
             with torch.no_grad():
                 teacher_outputs = self.teacher_model(
                     input_ids=student_outputs["sequences"],
                     attention_mask=attention_mask
                 )
-                teacher_logits = teacher_outputs.logits
-                log_ratio = (teacher_logits.log_softmax(-1).gather(-1, student_outputs["sequences"]) -
-                             student_logits.log_softmax(-1).gather(-1, student_outputs["sequences"])).sum(dim=1)
+                teacher_logits = teacher_outputs.logits[:, -gen_len-1:-1, :]
+                vocab_size = student_logits.shape[-1]
+                teacher_logits = teacher_logits.reshape(-1, vocab_size)
+                student_logits = student_logits.reshape(-1, vocab_size)
+                generated_token_ids = generated_token_ids.reshape(-1, 1)
+                log_ratio = (teacher_logits.log_softmax(-1).gather(-1, generated_token_ids) -
+                             student_logits.log_softmax(-1).gather(-1, generated_token_ids))
+                log_ratio = log_ratio.reshape(bsz, gen_len).sum(dim=1)[:, None]
 
-            loss = torch.nn.functional.cross_entropy(
-                student_logits / temperature, student_outputs["sequences"], reduction='none') * (log_ratio - 1)
-            loss = loss.mean()
+            cross_entropy = torch.nn.functional.cross_entropy(
+                student_logits / temperature, 
+                generated_token_ids.squeeze(-1), 
+                ignore_index=self.tokenizer.pad_token_id,
+                reduction='none').reshape(bsz, gen_len)
+            print("cross_entropy", cross_entropy.shape)
+            print("log_ratio", log_ratio.shape)
+            loss = cross_entropy * (log_ratio - 1)
+            loss = (loss * output_mask).sum() / output_mask.sum()
 
         if self.args.gradient_accumulation_steps > 1:
             loss = loss / self.args.gradient_accumulation_steps