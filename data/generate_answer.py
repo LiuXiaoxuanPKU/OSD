3a6f4a1701bf23659a7552d944cb16a1aeecfaf4
LiuXiaoxuanPKU <lilyliupku@gmail.com>
2023-09-17 20:31:33 +0000

    fix generate answer

--- data/generate_answer.py
+++ data/generate_answer.py
@@ -1,28 +1,30 @@
 import json
 from transformers import AutoTokenizer, LlamaForCausalLM
 from fastchat.model.model_adapter import get_conversation_template
+import torch
+from tqdm import tqdm
 
-def generate_answer(prompt):
-    model_path = "/rscratch/zhendong/lily/vicuna-7b-v1.3/"
-    model = LlamaForCausalLM.from_pretrained(model_path, device_map='auto', torch_dtype=torch.bfloat16)
-    tokenizer = AutoTokenizer.from_pretrained(model_path)
-    conv = get_conversation_template(model)
-    conv.append_message(conv.roles[0], prompt)
-    prompt_with_template = conv.get_prompt()
+model_path = "/data/vicuna-7b-v1.3/"
+model = LlamaForCausalLM.from_pretrained(model_path, device_map='auto', torch_dtype=torch.bfloat16)
+tokenizer = AutoTokenizer.from_pretrained(model_path)
     
+def generate_answer(prompt):
+    conv = get_conversation_template(model_path)
+    conv.append_message(conv.roles[0], prompt)
+    conv.append_message(conv.roles[1], "")
+    prompt_with_template = conv.get_prompt()
     max_new_tokens = 128
     inputs = tokenizer([prompt_with_template], return_tensors="pt").to(model.device)
-    generated = model.generate(**inputs, max_new_tokens=max_new_tokens)[0][inputs.input_ids.shape[-1]:]
+    generated = model.generate(**inputs, max_new_tokens=max_new_tokens)[0][inputs.input_ids.shape[-1]:-1]
     generated_str = tokenizer.decode(generated)
-    print(generated_str)
-    exit(0)
+    return generated_str
 
 
 def main(filename):
     with open(filename) as f:
         data = json.load(f)
 
-    for d in data:
+    for d in tqdm(data):
         assert len(d["conversation"]) == 1
         
         prompt = d["conversation"][0]["content"]
@@ -35,8 +37,8 @@ def main(filename):
         )
         
     with open(f"{filename.split('.')[0]}_with_answer.json", "w") as f:
-        f.dump(data)
+        json.dump(data, f)
 
 if __name__ == "__main__":
-    filename = ""
+    filename = "/home/lily/spec_new/data/spider_train.json"
     main(filename)
