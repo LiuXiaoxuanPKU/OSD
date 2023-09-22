9139296a39444a68d11571f3ecd49dad48e0e200
LiuXiaoxuanPKU <lilyliupku@gmail.com>
2023-09-22 20:18:06 +0000

    datapath

--- bash_scripts/online/all.sh
+++ bash_scripts/online/all.sh
@@ -1,5 +1,7 @@
-bash bash_scripts/online/sharp.sh /data/
-bash bash_scripts/online/smooth.sh /data/
-bash bash_scripts/online/sample_distill.sh /data/
-bash bash_scripts/online/sharp_baseline.sh /data/
-bash bash_scripts/online/smooth_baseline.sh /data/
+datapath=$1
+
+bash bash_scripts/online/sharp.sh $datapath
+bash bash_scripts/online/smooth.sh $datapath
+bash bash_scripts/online/sample_distill.sh $datapath
+bash bash_scripts/online/sharp_baseline.sh $datapath
+bash bash_scripts/online/smooth_baseline.sh $datapath
