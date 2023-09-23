58b847693e1b0c1a37e3f57e4d8d91982f759361
YangZhou08 <stevenzhou0816100@gmail.com>
2023-09-22 19:27:59 -0700

    gsm8k baseline scripts

--- bash_scripts/gsm8k/sample_online_distill.sh
+++ bash_scripts/gsm8k/sample_online_distill.sh
@@ -1,12 +1,11 @@
 datapath=$1
-WANDB_PROJECT=specInfer python distill/train.py \
-    --student_model_path JackFram/llama-160m\
+python distill/train.py \
+    --student_model_path $datapath/llama-160m\
     --teacher_model_path $datapath/vicuna-7b-v1.3/ \
-    --data_path data/spider_train_with_answer.json \
-    --eval_data_path data/spider_validation.json \
+    --data_path data/gsm8k_train_with_answer.json \
     --max_propose_num 5 \
     --bf16 True \
-    --output_dir $datapath/spider_online_distill\
+    --output_dir $datapath/gsm8k_online_distill\
     --num_train_epochs 0.1 \
     --per_device_train_batch_size 8 \
     --gradient_accumulation_steps 16 \
@@ -23,8 +22,38 @@ WANDB_PROJECT=specInfer python distill/train.py \
     --model_max_length 256 \
     --gradient_checkpointing True \
     --lazy_preprocess True \
-    --run_name spider_online_distill \
+    --run_name gsm8k_online_distill \
     --mode offline \
     --sample_source teacher \
     --kl_method forward \
     --report_to none
+
+WANDB_PROJECT=spec python distill/train.py \
+    --student_model_path $datapath/gsm8k_online_distill \
+    --teacher_model_path $datapath/vicuna-7b-v1.3/ \
+    --data_path data/gsm8k_train.json \
+    --max_propose_num 5 \
+    --bf16 True \
+    --output_dir $datapath/gsm8k_online_baseline\
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "epoch" \
+    --learning_rate 1e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0. \
+    --lr_scheduler_type "constant" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 256 \
+    --gradient_checkpointing True \
+    --lazy_preprocess True \
+    --run_name gsm8k_online_baseline \
+    --mode online \
+    --sample_source teacher \
+    --kl_method forward \
+    --online_eval_interval 1 \
+    --online_update_interval 100000 \
+    --logging_steps 1 \
+    --logging_nan_inf_filter true
