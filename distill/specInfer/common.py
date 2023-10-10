7f4c936e2744a00731349fb6dab04fbb519f6bbe
LiuXiaoxuanPKU <lilyliupku@gmail.com>
2023-10-09 17:22:03 -0700

    merge

--- distill/specInfer/common.py
+++ distill/specInfer/common.py
@@ -9,10 +9,11 @@ class InputAndCache:
     input_ids: torch.Tensor
     attention_mask: torch.Tensor
     past_key_values: torch.Tensor
-    
+
     # encoder-decoder only fields
     labels: torch.Tensor = None
     decoder_input_ids: torch.Tensor = None
+
 
 @dataclass
 class OutputAndCache:
@@ -51,6 +52,7 @@ def slice_past_key_values(past_key_values, start_idx, slice_len):
         )
     return tuple(new_past)
 
+
 def slice_past_key_values_seq2seq(past_key_values, start_idx, slice_len):
     new_past = []
     for idx in range(len(past_key_values)):
@@ -64,7 +66,7 @@ def slice_past_key_values_seq2seq(past_key_values, start_idx, slice_len):
                                         start_idx:start_idx+slice_len, :],
                 past_key_values[idx][3][:, :,
                                         start_idx:start_idx+slice_len, :],
-                
+
             )
         )
     return tuple(new_past)
@@ -82,6 +84,7 @@ def slice_mqa_past_key_values(past_key_values, start_idx, slice_len):
 def crop_past_key_values(past_key_values, max_len):
     return slice_past_key_values(past_key_values, 0, max_len)
 
+
 def crop_past_key_values_seq2seq(past_key_values, max_len):
     return slice_past_key_values_seq2seq(past_key_values, 0, max_len)
 
@@ -96,15 +99,20 @@ def sychronize_time():
 
 # convert a list of 1d tensors to a single 2d tensor
 # if those 1d tensors have different shapes, pad them to the longest length
+
+
 def pad_to_2d(tensor_list, pad_token_id, max_len=None):
-    if max_len == None:
-        max_len = max(tensor.shape[-1] for tensor in tensor_list)
+    if not isinstance(tensor_list[0], torch.Tensor):
+        tensor_list = [torch.tensor(t).reshape(1, -1) for t in tensor_list]
+    if max_len is None:
+        max_len = max([t.shape[-1] for t in tensor_list])
+    assert max_len > 0
 
     # Pad each tensor to the max length and stack them to form a 2D tensor
     result = torch.cat(
         [
             torch.nn.functional.pad(
-                tensor, (0, max_len - tensor.shape[-1]), 
+                tensor, (0, max_len - tensor.shape[-1]),
                 value=pad_token_id
             )
             for tensor in tensor_list
