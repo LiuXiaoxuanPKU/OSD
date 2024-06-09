f87ee8d0e69d0cad6d825287c5e0bdb8985506ae
lilyliu <lilyliupku@gmail.com>
2024-06-09 15:12:46 -0700

    update params

--- bash_scripts/spider/online.sh
+++ bash_scripts/spider/online.sh
@@ -1,7 +1,7 @@
 datapath=$1
 python distill/train.py \
-    --student_model_path $datapath/llama-160m \
-    --teacher_model_path $datapath/vicuna-7b-v1.3/ \
+    --student_model_path JackFram/llama-160m \
+    --teacher_model_path lmsys/vicuna-7b-v1.3 \
     --data_path data/raw_data/spider_train.json \
     --max_propose_num 5 \
     --bf16 True \
@@ -11,17 +11,17 @@ python distill/train.py \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "epoch" \
-    --learning_rate 1e-5 \
+    --learning_rate 1e-4 \
     --weight_decay 0. \
     --warmup_ratio 0. \
     --lr_scheduler_type "constant" \
     --tf32 True \
     --model_max_length 256 \
-    --gradient_checkpointing True \
+    --gradient_checkpointing False \
     --lazy_preprocess True \
     --run_name spider_online \
     --mode online \
     --online_eval_interval 1 \
-    --online_update_interval 1 \
+    --online_update_interval 8 \
     --logging_steps 1 \
     --logging_nan_inf_filter true
